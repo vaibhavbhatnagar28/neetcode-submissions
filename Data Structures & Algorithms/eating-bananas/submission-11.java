52b3a1e7fd7ab7b32e00f85aed9ffb63d7fbfd96
Vaibhav Bhatnagar <94589579+vaibhavbhatnagar28@users.noreply.github.com>
2026-05-23 17:35:10 +0530

    Add: eating-bananas - submission-11

---
+++ Data Structures & Algorithms/eating-bananas/submission-11.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1; int high = Arrays.stream(piles).max().getAsInt();
+        int res = high;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            long sum = 0;
+
+            for(int x: piles){
+                sum += Math.ceil((double)x/mid);
+            }
+            if(sum>h){
+                low = mid+1;
+            }
+            else{
+                res = mid;
+                high = mid-1;
+            }
+        }
+        return res;
+    }
+}
