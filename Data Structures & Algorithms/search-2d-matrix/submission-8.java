a6b3df2f36074c156ef1ca01d6b2bef90802bb5d
Vaibhav Bhatnagar <94589579+vaibhavbhatnagar28@users.noreply.github.com>
2026-05-25 23:06:22 +0530

    Add: search-2d-matrix - submission-8

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-8.java
@@ -0,0 +1,24 @@
+class Solution {
+        public boolean searchMatrix(int[][] matrix, int target) {
+                int m = matrix.length;
+                        int n = matrix[0].length;
+                                int col = n-1;
+                                        int row = 0;
+
+                                                while(row<m && col>=0){
+                                                            int temp = matrix[row][col];
+
+                                                                        if(temp == target){
+                                                                                        return true;
+                                                                                                    }
+                                                                                                                else if(temp <target){
+                                                                                                                                row++;
+                                                                                                                                            }
+                                                                                                                                                        else{
+                                                                                                                                                                        col--;
+                                                                                                                                                                                    }
+                                                                                                                                                                                            }
+                                                                                                                                                                                                    return false;
+                                                                                                                                                                                                        }
+                                                                                                                                                                                                        }
+
