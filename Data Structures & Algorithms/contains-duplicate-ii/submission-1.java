fc308e5fcf8a02e4c316368c977026f0cab77c3a
Vaibhav Bhatnagar <94589579+vaibhavbhatnagar28@users.noreply.github.com>
2026-08-13 08:59:15 +0530

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> window = new HashSet<>();
+        int L = 0;
+
+        for (int R = 0; R < nums.length; R++) {
+            if (R - L > k) {
+                window.remove(nums[L]);
+                L++;
+            }
+            if (window.contains(nums[R])) {
+                return true;
+            }
+            window.add(nums[R]);
+        }
+        return false;
+    }
+}
